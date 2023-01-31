e846d49b5d0771f49eb7368ca12aca2bdd6b3015
Leonid Karachevtsev <zebra-sherlok-holms@yandex.ru>
2023-01-31 23:36:41 +0300

    add actionButtons and delete old files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { HabbitsList } from './components/habbits-list';
+import { Header } from './components/header';
+
+export const App = () => {
+
+  return (
+    <div className="App">
+      <Header />
+      <HabbitsList />
+    </div>
+  );
+}
--- src/components/action-buttons/index.tsx
+++ src/components/action-buttons/index.tsx
@@ -1,6 +1,55 @@
-import { ActionButtonsProps } from "./types"
+import { useState, useRef } from "react";
+import { useOnClickOutside } from "usehooks-ts";
+import { ActionButtonsProps } from "./types";
+import "./actionButton.css";
 
 export const ActionButtons = ({ habbitId }: ActionButtonsProps) => {
+    const ref = useRef(null);
+    const [show, setShow] = useState(false);
+
+    const onClickDelete = (habbitId: string) => {
+        // TODO: use fetch
+        console.log('delete');
+        setShow(false);
+    }
+
+    const onClickEdit = (habbitId: string) => {
+        // TODO: use fetch
+        console.log('edit');
+        setShow(false);
+    }
+
+    const onClickHandler = () => {
+        setShow(true);
+    }
+    const onClickOutside = () => {
+        setShow(false);
+    }
+    useOnClickOutside(ref, onClickOutside);
+
+    const actions = [
+        {
+            id: 1,
+            title: "Изменить",
+            onClick: () => onClickEdit(habbitId)
+        },
+        {
+            id: 2,
+            title: "Удалить",
+            onClick: () => onClickDelete(habbitId)
+        },
+    ];
     
-    return (<div></div>)
+    return (
+        <div>
+            <button className="action-button" onClick={onClickHandler} ref={ref} >...</button>
+            { show && <div className="action-button_modal-container">
+                { actions.map((item) => 
+                    <div key={item.id} onClick={item.onClick} className="action-button_action">
+                        {item.title}
+                    </div>
+                )}
+            </div>}
+        </div>
+    )
 }
