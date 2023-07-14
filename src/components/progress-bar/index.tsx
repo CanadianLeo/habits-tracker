3d4e912beaa29f041a7474abce378c2ee53ad622
Leonid Karachevtsev <zebra-sherlok-holms@yandex.ru>
2023-07-14 19:43:30 +0300

    add decrement button in frontend

--- src/components/progress-bar/index.tsx
+++ src/components/progress-bar/index.tsx
@@ -16,22 +16,34 @@ export const ProgressBar = ({
   return (
     <div
       className={cn(
-        styles['progress-bar_wrapper'],
+        styles.wrapper,
         colors[`border-${color}`]
       )}
     >
+      <div 
+        className={cn(
+          styles['decrement-button'],
+          styles.button,
+          colors[`border-${color}`],
+          colors[`${color}-hovered`]
+        )}
+        onClick={onClickAdd}
+      >
+        -
+      </div>
       <div
         style={filledStyles}
         className={cn(
-          styles['progress-bar_wrapper'],
+          styles.wrapper,
           styles['filled'],
           colors[`${color}`]
         )}
       />
-      <div className={styles['progress-bar_title']}>{title}</div>
+      <div className={styles.title}>{title}</div>
       <div
         className={cn(
-          styles['progress-bar_add-button'],
+          styles['increment-button'],
+          styles.button,
           colors[`border-${color}`],
           colors[`${color}-hovered`]
         )}
