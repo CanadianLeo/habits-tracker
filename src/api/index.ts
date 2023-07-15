8f990bf70a57cba24880afeac82e62c4448db545
Leonid Karachevtsev <zebra-sherlok-holms@yandex.ru>
2023-07-15 13:58:58 +0300

    base_url to const

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import { Api, HttpClient } from "./Api";
+import { BASE_URL } from "./constants";
 
-const http = new HttpClient({baseUrl: 'http://localhost:4000/api'});
+const http = new HttpClient({baseUrl: BASE_URL});
 export const api = new Api(http);
