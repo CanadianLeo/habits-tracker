fe440ac381d1cd1e757369f0f328cfa330c79f2b
Leonid Karachevtsev <zebra-sherlok-holms@yandex.ru>
2023-07-15 14:16:00 +0300

    delete magic string

---
+++ src/components/habit-form/constants.ts
@@ -0,0 +1,10 @@
+export const FIELDS = {
+  name: {
+    id: 'name',
+    placeholder: 'Habit name',
+  },
+  description: {
+    id: 'description',
+    placeholder: 'Habit description',
+  }
+}
---
+++ src/components/periods/constants.ts
@@ -0,0 +1,2 @@
+export const REPEAT_CONTEXT = 'Repeat';
+export const TIMES_A = 'times a';
