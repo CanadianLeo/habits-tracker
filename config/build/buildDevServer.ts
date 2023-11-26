d1411cb30da82a1f980e3eaad3d8c0f803e24c1a
Leo <zebra-sherlok-holms@yandex.ru>
2023-11-26 17:15:06 +0300

    scss modules

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,5 +3,10 @@ import { BuildOptions } from './types/types';
 
 export const buildDevServer = (options: BuildOptions) => ({
     port: options.port ?? 3000,
-    open: true
+    open: true,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
 });
