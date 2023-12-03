9b56b45764a61987e526bef942540d232d97e0a3
Leo <zebra-sherlok-holms@yandex.ru>
2023-12-03 21:56:49 +0300

    use one textFiled

--- src/components/periods/index.tsx
+++ src/components/periods/index.tsx
@@ -3,11 +3,12 @@ import { useFormContext } from 'react-hook-form';
 import { Select } from 'components/select';
 import { PERIODS } from 'components/new-habit/constants';
 import { Period } from 'components/new-habit/types';
-import { REPEAT_CONTEXT, TIMES_A } from './constants';
+import { TextField } from 'components/text-field';
+import { TIMES_A, FIELD_REPEAT } from './constants';
 import styles from './styles.module.scss';
 
 export const Periods = () => {
-  const { register, setValue, getValues } = useFormContext();
+  const { setValue, getValues } = useFormContext();
 
   const onChangeHandle = (event: ChangeEvent<HTMLInputElement>) => {
     // Delete all characters that are not numbers
@@ -17,16 +18,7 @@ export const Periods = () => {
 
   return (
     <div className={styles.container}>
-      {REPEAT_CONTEXT}
-      <input
-        className={styles.input}
-        pattern='[0-9]*'
-        {...register('count', {
-          required: true,
-          pattern: /[0-9]*/,
-          onChange: onChangeHandle,
-        })}
-      />
+      <TextField {...FIELD_REPEAT} onChange={onChangeHandle} />
       {TIMES_A}
       <Select<Period> items={PERIODS} defaultItem={PERIODS[0]} renderValue='name' keyValue='id' />
     </div>
--- src/components/text-field/index.tsx
+++ src/components/text-field/index.tsx
@@ -1,12 +1,17 @@
+import { useMemo } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { TextFieldProps } from './types';
 import styles from './styles.module.scss';
 
-export const TextField = ({ id, placeholder }: TextFieldProps) => {
+export const TextField = ({ id, placeholder, required, pattern, onChange }: TextFieldProps) => {
   const { register } = useFormContext();
+  const stringPattern = useMemo(() => {
+    return pattern?.toString();
+  }, [pattern])
+
   return (
     <div className={styles['floating']}>
-      <input {...register(id, { required: true })} placeholder={placeholder} className={styles['input']} />
+      <input {...register(id, { required, pattern, onChange })} placeholder={placeholder} pattern={stringPattern} className={styles['input']} />
       <label className={styles['label']} htmlFor={id}>
         {placeholder}
       </label>
